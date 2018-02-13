ce565516786045d8e270b84a0d2ff2b3d5cf6856
Steven Gissubel <stevengissubel@rb-mac-007.home>
2018-02-13 07:37:09 -0500

    created a core folder - moved root level items into core - core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,19 @@ import { HttpModule } from '@angular/http';
 
 // my components
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+
+import { CoreModule } from './core/core.module'
 import { AppRoutingModule } from './app-routing.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 // my services 
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
-import { HomeComponent } from './home/home.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,9 +24,10 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     SharedModule,
     ShoppingListModule,
-    AuthModule
+    AuthModule,
+    CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
